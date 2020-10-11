57da2767d8b2391357e43fc410ad1d2cda7c4d8b
jieyinglei <5140790+jieyinglei@user.noreply.gitee.com>
2020-10-11 21:33:02 +0800

    登录页面修改

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,5 +3,12 @@ package com.example.demo.service;
 import com.example.demo.dto.UserSecurityDto;
 
 public interface UserService {
+    /**
+     * 注册用户
+     *
+     * @param user
+     */
     void userRegister(UserSecurityDto user);
+
+
 }
