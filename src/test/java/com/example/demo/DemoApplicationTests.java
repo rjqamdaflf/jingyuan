d7349afb7fc9792caf94c1323e11e926571f7845
rjqamdaflf <842712494@qq.com>
2020-10-20 00:10:36 +0800

    jfiejwq

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.dto.User;
-import com.example.demo.dto.UserSecurityDto;
-import com.example.demo.mapper.ApplicationDao;
-import com.example.demo.mapper.mysql.UserDao;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 
-import javax.annotation.Resource;
-import java.util.List;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DemoApplicationTests {
--- src/test/java/com/example/demo/entity/MyPersistentRememberMeTokenEntityTest.java
+++ src/test/java/com/example/demo/entity/MyPersistentRememberMeTokenEntityTest.java
@@ -3,8 +3,6 @@ package com.example.demo.entity;
 import com.example.demo.DemoApplicationTests;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class MyPersistentRememberMeTokenEntityTest extends DemoApplicationTests {
 
 
